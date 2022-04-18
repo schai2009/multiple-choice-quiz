3abfba33a1583e007273bbf973a2ab325893ef2e
schai2009 <schai20100@gmail.com>
2022-04-17 21:19:18 -0700

    Added HTML layout and created an array of questions for the quiz

---
+++ assets/java/script.js
@@ -0,0 +1,23 @@
+var questions = [
+    {
+        prompt: "What is the square root of 81?\n(1) 7\n(2) 9\n(3) 5\n(4) 11",
+        answer: "2"
+    },
+    {
+        prompt: "How many megabites are in one gigabite?\n(1) 512\n(2) 100\n(3) 1,000\n(4) 1,024",
+        answer: "3"
+    },
+    {
+        prompt: "What is the approximate diameter of the United States?\n(1) 5000 miles\n(2) 2,600 kilometers\n(3) 2,800 miles\n(4) 12,000 kilmoeters",
+        answer: "3"
+    },
+    {
+        prompt: "What is the average life expectancy of human in 2021?\n(1) 78.99 years\n(2) 76.52 years\n(3) 66.95 years\n(4) 85.36 years",
+        answer: "1"
+    },
+    {
+        prompt: "What is (1*0)+6/2+1?\n(1) 2\n(2) 4\n(3) 12\n(4) 0",
+        answer: "2"
+    },
+]
+
